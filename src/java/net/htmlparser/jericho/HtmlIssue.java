7b9bc88edf5e2cf32155c1ae8d397ffa027a7b21
Patrick Moore <transparentpolitics@gmail.com>
2010-10-13 20:07:13 +0000

    copyright
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/modified-opensource/jericho-html/trunk@205 9eb8ad66-390e-4894-8ab5-5ca26f751fd9

--- src/java/net/htmlparser/jericho/HtmlIssue.java
+++ src/java/net/htmlparser/jericho/HtmlIssue.java
@@ -1,7 +1,21 @@
-/**
- * Copyright 2006-2008 by Amplafi. All rights reserved.
- * Confidential. 
- */
+// Jericho HTML Parser - Java based library for analysing and manipulating HTML
+// Version 3.2-dev
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of either one of the following licences:
+//
+// 1. The Eclipse Public License (EPL) version 1.0,
+// included in this distribution in the file licence-epl-1.0.html
+// or available at http://www.eclipse.org/legal/epl-v10.html
+//
+// 2. The GNU Lesser General Public License (LGPL) version 2.1 or later,
+// included in this distribution in the file licence-lgpl-2.1.txt
+// or available at http://www.gnu.org/licenses/lgpl.txt
+//
+// This library is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied.
+// See the individual licence texts for more details.
+
 package net.htmlparser.jericho;
 
 
@@ -12,6 +26,12 @@ public class HtmlIssue {
     private final String message;
     private transient Source source;
 
+    public HtmlIssue(String message) {
+        this.message = message;
+        this.begin = null;
+        this.end = null;
+        this.priorToPosition = null;
+    }
     public HtmlIssue(Source source, int begin, String priorToPosition, String message) {
         this(source,begin,null,priorToPosition,message);
     }
